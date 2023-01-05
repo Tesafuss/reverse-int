9b5f38df10d0a4b2fbc40a1e0b249d5957b097a5
Tesafuss <d.Suhin94@gmail.com>
2023-01-05 15:40:16 +0300

    feat: solution of Revers int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
+
+  if(n < 0){
+    n = n * (-1);
+  }
+
+  let arr = Array.from(String(n));
+
+  for (let i = 0; i < (arr.length / 2); i++){
+    let x = arr.at(-1-i);
+    arr[arr.length-(1+i)] = arr[i];
+    arr[i] = x;
+  }
   
+  return Number(arr.join(''));
 }
